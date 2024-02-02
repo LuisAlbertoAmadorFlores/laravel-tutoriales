0231ac60be8d57f8a8c405615c4cfa322eb79f4a
Alberto Amador <alvertokarlos@gmail.com>
2024-02-01 20:18:15 -0600

    Task

--- todolist/routes/web.php
+++ todolist/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TaskController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+// Route::get('/',function(){
+//     return view('task.index');
+// });
+
+Route::get('/',[TaskController::class,'index']);
+
+Route::post('/',[TaskController::class,'store']);
+
+Route::delete('/{id}',[TaskController::class,'destroy'])->name('task.destroy');
